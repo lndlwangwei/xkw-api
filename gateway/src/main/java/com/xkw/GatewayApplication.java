adcc9ae1c05eb55ea05ede371176528632ae0294
wangwei <lndl.wangwei@gmail.com>
2019-07-22 18:16:00 +0800

    Initial commit

---
+++ gateway/src/main/java/com/xkw/GatewayApplication.java
@@ -0,0 +1,27 @@
+package com.xkw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route(p -> p
+                .path("/get")
+                .filters(f -> f.addRequestHeader("Hello", "World"))
+                .uri("https://www.baidu.com"))
+            .build();
+    }
+}
