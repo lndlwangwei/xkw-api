8bb5752f1970f222e312d753fcd8188cd518a085
wangwei <lndl.wangwei@gmail.com>
2019-07-23 09:55:48 +0800

    add test api

---
+++ test-api1/src/main/java/com/xkw/TestApi1Application.java
@@ -0,0 +1,13 @@
+package com.xkw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApi1Application.class, args);
+    }
+
+}
