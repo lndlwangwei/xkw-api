0060be8c2046d2ffd5d1b973b2b5f721c8bd1146
wangwei <lndl.wangwei@gmail.com>
2019-07-30 14:15:14 +0800

    jenkins test

--- jenkins/deploy_xkw_gateway.groovy
+++ jenkins/deploy_xkw_gateway.groovy
@@ -19,26 +19,52 @@
 //    }
 //}
 
-def envProp = load('jenkins/script/envProp.groovy')
-//envProp.nodes().each {nodeName ->
-//    node(nodeName) {
+//def envProp = load('jenkins/script/envProp.groovy')
+////envProp.nodes().each {nodeName ->
+////    node(nodeName) {
+////        echo nodeName
+////    }
+////}
+//def branches = [:]
+//for (int i = 0; i < 2; i++) {
+//    def nodeName
+//    if (i == 0) nodeName = 'dev'
+//    else nodeName = '28test'
+//
+//    echo nodeName
+//
+//    branches[nodeName] = node(nodeName) {
 //        echo nodeName
 //    }
 //}
-def branches = [:]
-for (int i = 0; i < 2; i++) {
-    def nodeName
-    if (i == 0) nodeName = 'dev'
-    else nodeName = '28test'
+//
+//parallel branches
 
-    echo nodeName
 
-    branches[nodeName] = node(nodeName) {
-        echo nodeName
+def stringsToEcho = ["a", "b", "c", "d"]
+
+// The map we'll store the parallel steps in before executing them.
+def stepsForParallel = stringsToEcho.collectEntries {
+    ["echoing ${it}" : transformIntoStep(it)]
+}
+
+// Actually run the steps in parallel - parallel takes a map as an argument,
+// hence the above.
+parallel stepsForParallel
+
+// Take the string and echo it.
+def transformIntoStep(inputString) {
+    // We need to wrap what we return in a Groovy closure, or else it's invoked
+    // when this method is called, not when we pass it to parallel.
+    // To do this, you need to wrap the code below in { }, and either return
+    // that explicitly, or use { -> } syntax.
+    return {
+        node {
+            echo inputString
+        }
     }
 }
 
-parallel branches
 
 //node('28test') {
 //    git 'https://github.com/lndlwangwei/xkw-api'
