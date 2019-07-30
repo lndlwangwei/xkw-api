6eab1346b918843b3c66039a91baf8855ccf984b
wangwei <lndl.wangwei@gmail.com>
2019-07-30 11:09:14 +0800

    jenkins test

--- jenkins/deploy_xkw_gateway.groovy
+++ jenkins/deploy_xkw_gateway.groovy
@@ -1,5 +1,5 @@
 node('dev') {
-    def externalMethod = load("jenkins/script/externalScript.groovy")
+    def externalMethod = load("jenkins/externalScript.groovy")
 
     externalMethod.sayHello('wangwei')
     echo 'hello wangwei'
