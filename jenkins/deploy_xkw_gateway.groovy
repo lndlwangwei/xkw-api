e0b01efd2c841f5b5e2ffb8f2fc5a9c819a9e2c7
wangwei <lndl.wangwei@gmail.com>
2019-07-31 13:52:49 +0800

    jenkins test

--- jenkins/deploy_xkw_gateway.groovy
+++ jenkins/deploy_xkw_gateway.groovy
@@ -3,25 +3,26 @@ def serviceBasePath = '/data/service/gateways'
 def buildProjectName = "xkw-api-gateway-build"
 
 nodes.entrySet().each {entry ->
-    def nodeName = entry.key
+    def env = entry.key
     def services = entry.value
 
-    node(nodeName) {
+    node(env) {
         stage('deploy') {
             deleteDir('target')
             copyArtifacts(projectName: "${buildProjectName}")
 
-            services.each {
-                def servicePath = "$serviceBasePath/$it"
+            services.each { service ->
+                def servicePath = "$serviceBasePath/$service"
                 if (!fileExists(servicePath)) {
                     sh "mkdir ${servicePath}"
-                    writeFile encoding: 'utf-8', file: "$servicePath/test.txt", text: "this is $it"
+                    writeFile encoding: 'utf-8', file: "$servicePath/test.txt", text: "this is $service"
                 }
 
                 sh "cp target/*.jar ${servicePath}"
 
+                    def serviceIndex = service.substring(service.length() - 2)
 //                withEnv(['JENKINS_NODE_COOKIE=dontkillme']) {
-                    sh "java -jar ${servicePath}/gateway-0.0.1-SNAPSHOT.jar &"
+                    sh "java -jar ${servicePath}/gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=node$serviceIndex &"
 //                }
             }
         }
--- jenkins/script/envProp.groovy
+++
@@ -1,5 +0,0 @@
-def Clasters() {
-    return ['dev': ['gateway1', 'gateway2', 'gateway3'], '28test':['gateway1', 'gateway2']]
-}
-
-return this
