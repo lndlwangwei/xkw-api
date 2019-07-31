d6947585244016d7620d214751ea2eccd49a21ad
wangwei <lndl.wangwei@gmail.com>
2019-07-31 19:12:33 +0800

    jenkins test

--- jenkins/deploy_xkw_gateway.groovy
+++ jenkins/deploy_xkw_gateway.groovy
@@ -17,20 +17,20 @@ nodes.entrySet().each {entry ->
             services.each { service ->
                 def serviceIndex = Integer.parseInt(service.substring(service.length() - 1))
 
-//                sh "curl localhost:807$serviceIndex/offline"
-//                sh "sleep 1m"
+                sh "curl localhost:807$serviceIndex/offline"
+                sh "sleep 1m"
 
-                def servicePath = "$serviceBasePath/$service"
-                if (!fileExists(servicePath)) {
-                    sh "mkdir ${servicePath}"
-                    writeFile encoding: 'utf-8', file: "$servicePath/test.txt", text: "this is $service"
-                }
-
-                sh "cp target/*.jar ${servicePath}"
-
-                withEnv(['JENKINS_NODE_COOKIE=dontkillme']) {
-                    sh "java -jar ${servicePath}/gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=node$serviceIndex &"
-                }
+//                def servicePath = "$serviceBasePath/$service"
+//                if (!fileExists(servicePath)) {
+//                    sh "mkdir ${servicePath}"
+//                    writeFile encoding: 'utf-8', file: "$servicePath/test.txt", text: "this is $service"
+//                }
+//
+//                sh "cp target/*.jar ${servicePath}"
+//
+//                withEnv(['JENKINS_NODE_COOKIE=dontkillme']) {
+//                    sh "java -jar ${servicePath}/gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=node$serviceIndex &"
+//                }
             }
         }
     }
