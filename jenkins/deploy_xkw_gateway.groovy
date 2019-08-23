c3a53df59f460e0378d36c5df22f3edaf474c753
wangwei <lndl.wangwei@gmail.com>
2019-08-23 17:54:31 +0800

    测试jenkins 端口占用脚本

--- jenkins/deploy_xkw_gateway.groovy
+++ jenkins/deploy_xkw_gateway.groovy
@@ -19,7 +19,7 @@ nodes.entrySet().each {entry ->
                 def profile = service.equals("temp") ? 'temp' : "node$serviceIndex"
 
                 warnError(message: 'service is not online') {
-                    def portStatus = sh("lsof -i:807$serviceIndex")
+                    def portStatus = sh("num=`echo -n '\n'|telnet localhost 807$serviceIndex|grep Connected|wc -l`")
                     echo "port status: $portStatus"
 
                     // 将服务从eureka server中主动下线
