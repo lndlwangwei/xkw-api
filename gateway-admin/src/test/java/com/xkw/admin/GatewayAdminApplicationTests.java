c81e76c9db44d7d0f20db969db29d80e1ed20f7d
wangwei <lndl.wangwei@gmail.com>
2019-08-28 12:15:43 +0800

    测试jenkins脚本

--- gateway-admin/src/test/java/com/xkw/admin/GatewayAdminApplicationTests.java
+++ gateway-admin/src/test/java/com/xkw/admin/GatewayAdminApplicationTests.java
@@ -34,7 +34,7 @@ public class GatewayAdminApplicationTests {
     @Test
     public void contextLoads() throws InterruptedException {
 
-        for (int j = 0; j < 100; j++) {
+        for (int j = 0; j < 1; j++) {
             new Thread(() -> {
                 for (int i = 0; i < 10000; i++) {
                     try {
