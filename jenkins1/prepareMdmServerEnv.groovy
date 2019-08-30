9043f7bbd88fad4bb3e51baa523494b36ced053b
wangwei <lndl.wangwei@gmail.com>
2019-08-30 17:44:25 +0800

    test jenkins and docker

--- jenkins1/prepareMdmServerEnv.groovy
+++ jenkins1/prepareMdmServerEnv.groovy
@@ -28,8 +28,7 @@ node('28test') {
 
 //        sh "docker pull $nginxDockerImageName"
 
-        def existContainer = sh(script: "docker rm $nginxContainerName", returnStatus: true)
-        echo "$existContainer"
+        sh(script: "docker rm $nginxContainerName", returnStatus: true)
 
         sh "docker run -d -p 9080:9080 -v $nginxLogPath:/var/log/nginx -v $nginxBasePath/conf:/etc/nginx/ --name=$nginxContainerName xuekewang/jetty-9:v1"
     }
