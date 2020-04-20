006bc85bad3f41932f6f66b0e563ab8ce6233b7a
wangwei <lndl.wangwei@gmail.com>
2020-04-21 01:25:24 +0800

    Fix: ngx-bootstrap报错修复

--- jenkins/gatewayWebsiteDeploy.groovy
+++ jenkins/gatewayWebsiteDeploy.groovy
@@ -3,7 +3,7 @@ def nodeName = currentBuild.projectName.find(/\(([^\)]+)\)/, {group -> group[1]}
 def env = [
         buildProjectName: 'xkw-gateway-build(159test)',
         appDir: '/data/apps/xkw-gateway-client',
-        artifactName: 'gateway-admin-client/gateway_client.zip',
+        artifactName: 'gateway_client.zip',
 ]
 
 node(nodeName) {
@@ -11,7 +11,7 @@ node(nodeName) {
         copyArtifacts(projectName: "${env.buildProjectName}")
 
         sh "rm -rf ${env.appDir}/*"
-        sh "cp ${env.artifactName} ${env.appDir}"
+        sh "cp ${gateway-admin-client}/${env.artifactName} ${env.appDir}"
         sh "unzip ${env.appDir}/${env.artifactName} -d ${env.appDir}"
         sh "rm -f ${env.appDir}/${env.artifactName}"
     }
